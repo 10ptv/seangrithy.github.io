2e33dc753173ee0660dad882d6396ac21c2634df
10ptv <gtaplayer720@gmail.com>
2023-12-18 01:20:30 -0800

    dfsaf

---
+++ member/login.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Create a database connection
+    $con = new mysqli('localhost', 'root', '', 'loginform');
+
+    // Check for connection errors
+    if ($con->connect_error) {
+        die('Connection failed: ' . $con->connect_error);
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $sql = "SELECT username, password FROM users WHERE username=?";
+    $stmt = $con->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($dbUsername, $dbPassword);
+            $stmt->fetch();
+
+            // Verify the entered password with the hashed password from the database
+            if (password_verify($password, $dbPassword)) {
+                // Password is correct, set the username in the session
+                $_SESSION['username'] = $dbUsername;
+                echo "Password is correct";
+
+                // Redirect to the logged-in page
+                header('Location: login_process.php');
+                exit;
+            } else {
+                // Incorrect password, handle failed login
+                echo "Incorrect password. Please try again.";
+            }
+        } else {
+            // Username not found, handle failed login
+            echo "Username not found. Please try again.";
+        }
+
+        $stmt->close();
+    } else {
+        die('Error preparing statement: ' . $con->error);
+    }
+
+    $con->close();
+}
+
+// Your HTML for the login form goes here
+?>
---
+++ member/signup.php
@@ -0,0 +1,71 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
+
+    // Validate and sanitize inputs (you may add more validation as needed)
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+    $password = filter_var($password, FILTER_SANITIZE_STRING);
+    $confirmPassword = filter_var($confirmPassword, FILTER_SANITIZE_STRING);
+
+    // Check if passwords match
+    if ($password !== $confirmPassword) {
+        echo "Error: Passwords do not match. Please try again.";
+        exit;
+    }
+
+    // Hash the password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Create a database connection
+    $con = new mysqli('localhost', 'root', '', 'loginform');
+
+    // Check for connection errors
+    if ($con->connect_error) {
+        die('Connection failed: ' . $con->connect_error);
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $checkExistingUserSql = "SELECT username FROM users WHERE username=?";
+    $checkStmt = $con->prepare($checkExistingUserSql);
+
+    if ($checkStmt) {
+        $checkStmt->bind_param('s', $username);
+        $checkStmt->execute();
+        $checkStmt->store_result();
+
+        if ($checkStmt->num_rows > 0) {
+            echo "Error: The username '" . $username . "' already exists. Please choose a different username.";
+        } else {
+            // Insert the new user with hashed password
+            $insertUserSql = "INSERT INTO `users` (username, password) VALUES (?, ?)";
+            $insertStmt = $con->prepare($insertUserSql);
+
+            if ($insertStmt) {
+                $insertStmt->bind_param('ss', $username, $hashedPassword);
+
+                if ($insertStmt->execute()) {
+                    echo "Sign up successful! Welcome, $username.";
+                } else {
+                    echo "Error inserting data: " . $insertStmt->error;
+                }
+
+                $insertStmt->close();
+            } else {
+                echo "Error preparing statement: " . $con->error;
+            }
+        }
+
+        $checkStmt->close();
+    } else {
+        die('Error preparing statement: ' . $con->error);
+    }
+
+    $con->close();
+} else {
+    // Redirect or handle the case when someone tries to access this page directly without submitting the form.
+    echo "Invalid request.";
+}
+?>
