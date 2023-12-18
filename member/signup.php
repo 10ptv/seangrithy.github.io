f9d413ca1a9da1e7093710a8ed6617fdd5941aac
10ptv <gtaplayer720@gmail.com>
2023-12-18 02:00:20 -0800

    asdf

--- member/signup.php
+++ member/signup.php
@@ -1,71 +1,50 @@
 <?php
+// Start the session
+session_start();
 
+// Database connection parameters
+$host = 'sql12.freesqldatabase.com';
+$username = 'sql12671198';
+$password = 'HTeIXZJhFS';
+$database = 'sql12671198';
+
+// Create a database connection
+$con = new mysqli($host, $username, $password, $database);
+
+// Check for connection errors
+if ($con->connect_error) {
+    die('Connection failed: ' . $con->connect_error);
+}
+
+// Assuming you have received form data from POST method
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
     $username = $_POST['username'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirm_password'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);  // Hash the password
 
-    // Validate and sanitize inputs (you may add more validation as needed)
-    $username = filter_var($username, FILTER_SANITIZE_STRING);
-    $password = filter_var($password, FILTER_SANITIZE_STRING);
-    $confirmPassword = filter_var($confirmPassword, FILTER_SANITIZE_STRING);
+    // Perform the signup operation
+    $sql = "INSERT INTO users (name, username, password) VALUES (?, ?, ?)";
+    $stmt = $con->prepare($sql);
 
-    // Check if passwords match
-    if ($password !== $confirmPassword) {
-        echo "Error: Passwords do not match. Please try again.";
-        exit;
-    }
+    if ($stmt) {
+        $stmt->bind_param('sss', $name, $username, $password);
+        $result = $stmt->execute();
 
-    // Hash the password
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    // Create a database connection
-    $con = new mysqli('sql12.freesqldatabase.com', 'sql12671198', 'HTeIXZJhFS', 'sql12671198');
-
-    // Check for connection errors
-    if ($con->connect_error) {
-        die('Connection failed: ' . $con->connect_error);
-    }
-
-    // Use prepared statements to prevent SQL injection
-    $checkExistingUserSql = "SELECT username FROM users WHERE username=?";
-    $checkStmt = $con->prepare($checkExistingUserSql);
-
-    if ($checkStmt) {
-        $checkStmt->bind_param('s', $username);
-        $checkStmt->execute();
-        $checkStmt->store_result();
-
-        if ($checkStmt->num_rows > 0) {
-            echo "Error: The username '" . $username . "' already exists. Please choose a different username.";
+        if ($result) {
+            // Signup successful
+            echo "Signup successful";
         } else {
-            // Insert the new user with hashed password
-            $insertUserSql = "INSERT INTO `users` (username, password) VALUES (?, ?)";
-            $insertStmt = $con->prepare($insertUserSql);
-
-            if ($insertStmt) {
-                $insertStmt->bind_param('ss', $username, $hashedPassword);
-
-                if ($insertStmt->execute()) {
-                    echo "Sign up successful! Welcome, $username.";
-                } else {
-                    echo "Error inserting data: " . $insertStmt->error;
-                }
-
-                $insertStmt->close();
-            } else {
-                echo "Error preparing statement: " . $con->error;
-            }
+            // Error in the signup process
+            echo "Error: " . $stmt->error;
         }
 
-        $checkStmt->close();
+        $stmt->close();
     } else {
-        die('Error preparing statement: ' . $con->error);
+        // Error preparing the statement
+        echo 'Error preparing statement: ' . $con->error;
     }
-
-    $con->close();
-} else {
-    // Redirect or handle the case when someone tries to access this page directly without submitting the form.
-    echo "Invalid request.";
 }
+
+// Close the database connection
+$con->close();
 ?>
